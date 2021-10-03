c3db1cccb3be55eef32de8ad3d93d1ae65151b39
Laura Endreß <laura.endress@syntaxphoenix.com>
2021-10-03 17:32:05 +0200

    Remove @Rentention

--- module/src/main/java/com/syntaxphoenix/avinity/module/extension/ExtensionPoint.java
+++ module/src/main/java/com/syntaxphoenix/avinity/module/extension/ExtensionPoint.java
@@ -1,12 +1,9 @@
 package com.syntaxphoenix.avinity.module.extension;
 
 import static java.lang.annotation.ElementType.TYPE;
-import static java.lang.annotation.RetentionPolicy.CLASS;
 
-import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
-@Retention(CLASS)
 @Target(TYPE)
 public @interface ExtensionPoint {
 
